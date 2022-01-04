0e8c20382676c7495074b35216bbc758e7a599f6
JanBinkowski <jan.binkowski@wp.pl>
2022-01-04 15:42:42 +0100

    commit with different options of delimeter and adding one role to multiple fields

--- parser.py
+++ parser.py
@@ -2,9 +2,15 @@ import numpy as np
 import csv
 import sys
 
+
 class CSV_Parser:
+    global end_file_name
+    end_file_name =  'PARSED_CSV.csv'
+
+
     def __init__(self, file_name):
         self.file_name = file_name
+
 
     def read_csv(self, path):
         with open(path, 'r') as f:
@@ -13,8 +19,10 @@ class CSV_Parser:
 
         return table
 
-    def transform_to_key_value_pairs(self):
-        table = self.read_csv(str(self.file_name))
+
+    def extract_users_and_roles(self):
+        global table
+        table= self.read_csv(str(self.file_name))
         business_roles = []
         users = []
 
@@ -24,19 +32,63 @@ class CSV_Parser:
         for every_user in range(1, table.shape[0]):
             users = np.append(users, table[every_user, 0])
 
+        return users, business_roles
+
+
+    def transform_to_key_value_pairs(self, mode):
+        users, business_roles = self.extract_users_and_roles()
+
         for iter_in_every_user in range(0,len(users)+1):
             for iter_in_every_business_role in range(1, len(business_roles) + 1):
                 if table[iter_in_every_user, iter_in_every_business_role] == 'x':
-                    output_string = str(users[iter_in_every_user - 1]) + ',' + str(business_roles[iter_in_every_business_role - 1]) + '\n'
-                    self.save_as_csv(output_string, 'PARSED_CSV.csv')
+                    if mode == 'przecinek':
+                        output_string = str(users[iter_in_every_user - 1]) + ',' + str(
+                            business_roles[iter_in_every_business_role - 1]) + '\n'
+                    elif mode == 'srednik':
+                        output_string = str(users[iter_in_every_user - 1]) + ';' + str(
+                            business_roles[iter_in_every_business_role - 1]) + '\n'
+                    self.save_as_csv(output_string, end_file_name)
+
 
     def save_as_csv(self, input_text, file_name):
         with open(file_name, 'a') as file:
             file.write(input_text)
 
+
+    def add_role_to_all_users(self,role_name):
+        users, business_roles = self.extract_users_and_roles()
+        for iter_in_every_user in range(0,len(users)+1):
+            if iter_in_every_user > 0:
+                output_string = users[iter_in_every_user - 1] + ';' + str(role_name) + '\n'
+                self.save_as_csv(output_string, end_file_name)
+
 def main():
     Parser = CSV_Parser(sys.argv[1])
-    Parser.transform_to_key_value_pairs()
+    ans = True
+    while ans:
+        print("""
+        1.Parsuj dane z oddzileaczem jako srednik (dla User Self Service) 
+        2.Parsuj dane z oddzileaczem jako przecinek (dla Maintain Business Users/Roles)
+        3.Dopisz 1 role do kazdego usera w pliku .csv
+        4.Wyjdz
+        """)
+        ans = input()
+        if ans == "1":
+            Parser.transform_to_key_value_pairs('srednik')
+            print('\n Dane sparsowane pomyslnie. Zapisane jako plik',str(end_file_name),'\n')
+        elif ans == "2":
+            Parser.transform_to_key_value_pairs('przecinek')
+            print('\n Dane sparsowane pomyslnie. Zapisane jako plik',str(end_file_name),'\n')
+        elif ans == "3":
+            role_name = input('Wprowadz nazwe roli: ')
+            if len(role_name) != 0:
+                Parser.add_role_to_all_users(role_name)
+                print('\nRola przypisana pomyslnie. Zapisane jako',str(end_file_name),'\n')
+        elif ans == "4":
+            print("\n Zamykanie ....\n")
+            break
+        elif ans != "":
+            print("\n Niepoprawny wybor. Sprobuj ponownie.")
 
 if __name__ == '__main__':
     main()
